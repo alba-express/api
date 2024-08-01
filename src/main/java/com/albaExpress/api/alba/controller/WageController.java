fb2cd857f7cd88a2675cc5d48956a4042d71c3e2
sedica3340 <sedica3340@gmail.com>
2024-08-01 14:00:29 +0900

    요청확인중 아직 실패

--- src/main/java/com/albaExpress/api/alba/controller/WageController.java
+++ src/main/java/com/albaExpress/api/alba/controller/WageController.java
@@ -3,6 +3,7 @@ package com.albaExpress.api.alba.controller;
 import com.albaExpress.api.alba.entity.SalaryLog;
 import com.albaExpress.api.alba.service.WageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/wage")
 @RequiredArgsConstructor
+@Slf4j
 public class WageController {
 
     private final WageService wageService;
 
-
     @GetMapping("/workplace")
     public ResponseEntity<?> wageMainGet(@RequestParam String workplaceId) {
-
+        log.info("요청들어옴!");
         List<SalaryLog> salaryLogList = wageService.getSalaryLogInWorkplace(workplaceId);
 
 
