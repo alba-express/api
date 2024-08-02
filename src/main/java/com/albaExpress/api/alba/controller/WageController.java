3a0db020d56cca7aa494d61ea7e659a64c0c24e1
sedica3340 <sedica3340@gmail.com>
2024-08-02 11:28:23 +0900

    QueryDslConfig.java

--- src/main/java/com/albaExpress/api/alba/controller/WageController.java
+++ src/main/java/com/albaExpress/api/alba/controller/WageController.java
@@ -1,15 +1,11 @@
 package com.albaExpress.api.alba.controller;
 
 import com.albaExpress.api.alba.dto.request.SalaryAmountRequestDto;
-import com.albaExpress.api.alba.entity.SalaryLog;
 import com.albaExpress.api.alba.service.WageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.YearMonth;
-import java.util.List;
 
 @RestController
 @RequestMapping("/wage")
@@ -20,9 +16,9 @@ public class WageController {
     private final WageService wageService;
 
     @PostMapping("/workplace")
-    public ResponseEntity<?> wageMainPost(@RequestBody SalaryAmountRequestDto dto) {
+    public ResponseEntity<?> wageMainPost(@RequestBody SalaryAmountRequestDto reqDto) {
         log.info("요청들어옴!");
-        int salaryAmount = wageService.getSalaryLogInWorkplace(dto.getWorkplaceId(), dto.getYm());
+        int salaryAmount = wageService.getSalaryLogInWorkplace(reqDto.getWorkplaceId(), reqDto.getYm());
 
         return ResponseEntity.ok().body(salaryAmount);
     }
