6e51d926609b8615a5c80ed624e34ff86d6ad77a
junglimit <jaehan_jjh@naver.com>
2024-08-22 15:51:15 +0900

    일별 스케줄로그 조회기능 추가

--- src/main/java/com/albaExpress/api/alba/dto/response/SlaveDto.java
+++ src/main/java/com/albaExpress/api/alba/dto/response/SlaveDto.java
@@ -6,16 +6,25 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class SlaveDto {
     private String id;
     private String slaveName;
     private String slavePosition;
     private String scheduleStart; // 출근 시간
     private String scheduleEnd;   // 퇴근 시간
+    private String status;        // 근무 상태 (출근 전, 근무 중, 퇴근)
 
     public SlaveDto(String id, String slaveName) {
         this.id = id;
         this.slaveName = slaveName;
+    }
+
+    public SlaveDto(String id, String slaveName, String slavePosition, String scheduleStart, String scheduleEnd, String status) {
+        this.id = id;
+        this.slaveName = slaveName;
+        this.slavePosition = slavePosition;
+        this.scheduleStart = scheduleStart;
+        this.scheduleEnd = scheduleEnd;
+        this.status = status;
     }
 }
